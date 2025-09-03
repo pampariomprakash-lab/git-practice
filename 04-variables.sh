77d5af5362a43a5e3ca42e3effe96faf4f4b4735
Omi <Pampari.omprakash@gmail.com>
2025-09-03 19:07:39 +0530

    using multiples variables

--- 04-variables.sh
+++ 04-variables.sh
@@ -3,5 +3,7 @@
 PERSON1=$1
 PERSON2=$2
 PERSON3=$3
+PERSON4=$5
+PERSON5=$6
 echo "$PERSON1 & $PERSON3:: I started learnig Devops, how about you shravan?"
-echo "$PERSON2:: Good to hear i am also planning to learn Devops"
+echo "$PERSON2 & $PERSON5 & $PERSON4:: Good to hear i am also planning to learn Devops"
