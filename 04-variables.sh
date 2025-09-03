4a828afe2d1c95e8b4cf7e76ca325346c7477f0f
Omi <Pampari.omprakash@gmail.com>
2025-09-03 19:03:28 +0530

    trying to use two variables

--- 04-variables.sh
+++ 04-variables.sh
@@ -3,5 +3,5 @@
 PERSON1=$1
 PERSON2=$2
 PERSON3=$3
-echo "$PERSON1:: I started learnig Devops, how about you shravan?"
+echo "$PERSON1 & $PERSON3:: I started learnig Devops, how about you shravan?"
 echo "$PERSON2:: Good to hear i am also planning to learn Devops"
