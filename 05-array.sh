fd4a7a46949ec44597edde8c70688ea4a5a5077f
Omi <Pampari.omprakash@gmail.com>
2025-09-03 19:45:41 +0530

    trying arrays

---
+++ 05-array.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+FRUITS=("APPLE" "KIWI" "MANGO")
+
+echo "First fruit is $FRUITS[0]"
+echo "Second fruit is $FRUITS[1]"
+echo "First fruit is $FRUITS[2]"
+
+echo "All fruits together are $FRUITS"
+ 
+ read "what fruit do you want"
+
+ echo "thank you for chosing the fruit"
+
+ read "the fruit enetered is "
