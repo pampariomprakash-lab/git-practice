4d16de2b143a796eac00ff8262b20185624f63b1
Omi <Pampari.omprakash@gmail.com>
2025-09-04 10:29:28 +0530

    error handling

--- 05-array.sh
+++ 05-array.sh
@@ -6,7 +6,7 @@ echo "First fruit is ${FRUITS[0]}"
 echo "Second fruit is ${FRUITS[1]}"
 echo "First fruit is ${FRUITS[2]}"
 
-echo "All fruits together are $FRUITS"
+echo "All fruits together are ${FRUITS[@]}"
  
 echo "what fruit do you want"
 read -s FRUITS
