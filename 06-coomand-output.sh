23437fb19ee82a2384cfebaea83dc3f9859ef89b
Omi <Pampari.omprakash@gmail.com>
2025-09-04 11:03:08 +0530

    error handling

--- 06-coomand-output.sh
+++ 06-coomand-output.sh
@@ -2,3 +2,6 @@
 DATE=$(date & cal)
 
 echo "todays day & Month is $DATE "
+
+#how to run a command in a script
+#we can keep the command inside the $braces and can use the command wereever u want
---
+++ 07-data-types.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+NUMBER1=$1
+NUMBER2=$2
+read enter the numbers $NUMBER1 $NUMBER2
+
+SUM=$(($NUMBER1+$NUMBER2))
