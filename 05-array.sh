edfd273c59f77c49e268ac63fb59276f23b23e9c
Omi <Pampari.omprakash@gmail.com>
2025-09-04 10:27:28 +0530

    error handling

--- 05-array.sh
+++ 05-array.sh
@@ -2,9 +2,9 @@
 
 FRUITS=("APPLE" "KIWI" "MANGO")
 
-echo "First fruit is $FRUITS[0]"
-echo "Second fruit is $FRUITS[1]"
-echo "First fruit is $FRUITS[2]"
+echo "First fruit is ${FRUITS[0]}"
+echo "Second fruit is ${FRUITS[1]}"
+echo "First fruit is ${FRUITS[2]}"
 
 echo "All fruits together are $FRUITS"
  
