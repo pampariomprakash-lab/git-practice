5c27b4f64e7c1331812824681e5ec6ff895c7f46
Omi <Pampari.omprakash@gmail.com>
2025-09-04 10:14:50 +0530

    error handling

--- 05-array.sh
+++ 05-array.sh
@@ -8,8 +8,8 @@ echo "First fruit is $FRUITS[2]"
 
 echo "All fruits together are $FRUITS"
  
-read "what fruit do you want"
+read what fruit do you want
 
-echo "thank you for chosing the fruit"
+echo "thank you for chosing the $FRUITS[2]"
 
-read "the fruit enetered is "
+read the fruit enetered is 
