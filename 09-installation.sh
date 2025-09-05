6dcc64244092f6e93686e3602b6887d83496cbfe
Omi <Pampari.omprakash@gmail.com>
2025-09-05 10:00:25 +0530

    trying conditions

--- 09-installation.sh
+++ 09-installation.sh
@@ -17,7 +17,14 @@ if [ $? -ne 0 ]
 then   
     echo " Git is not installed , going to install it.."
     dnf install git -y
+    if [ $? -ne 0]
+    then
+        echo " Git installation is not successfull,, recheck it"
+        exit 1
+    else
+        echo " git installation is successfull"
+    fi
 else
-    echo " it is already installed "
+    echo " it is already installed nothing to do "
 
 fi
