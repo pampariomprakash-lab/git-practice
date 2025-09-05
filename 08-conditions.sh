4ff13bdbf7ea103a7a84e130846bf9dea6d7455f
Omi <Pampari.omprakash@gmail.com>
2025-09-05 08:32:40 +0530

    trying conditions

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -4,8 +4,8 @@ NUMBER=$1
 
 if [ NUMBER -gt 20 ]
 then
-echo " the $NUMBER is greater thatn 20 "
+    echo " the $NUMBER is greater thatn 20 "
 else 
-echo " the $NUMBER is less than 20 "
+    echo " the $NUMBER is less than 20 "
 
 fi
