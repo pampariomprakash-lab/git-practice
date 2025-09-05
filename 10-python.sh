2cdbc602b6a5b7633bd78b1a8fa9a13f36ad1c74
Omi <Pampari.omprakash@gmail.com>
2025-09-05 20:30:51 +0530

    trying conditions

--- 10-python.sh
+++ 10-python.sh
@@ -7,6 +7,9 @@ if [ $USERID -eq 0 ]
     then 
     dnf install python -y
     echo "Python is being installed "
+   if  [ dnf list installed python ]
+   then exit 1
     else
     echo " run the script with sudo access"
+fi
 fi    
