4c9ad16a4b9e5c66de02d90bca0f0e3073234aaf
Omi <Pampari.omprakash@gmail.com>
2025-09-05 09:57:08 +0530

    trying conditions

--- 09-installation.sh
+++ 09-installation.sh
@@ -10,3 +10,14 @@ then
     else
     echo " you don't have root access "
 fi
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then   
+    echo " Git is not installed , going to install it.."
+    dnf install git -y
+else
+    echo " it is already installed "
+
+fi
