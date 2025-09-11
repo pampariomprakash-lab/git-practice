d47250c23cdc709bd990d6b400737175ee13205d
Omi <Pampari.omprakash@gmail.com>
2025-09-11 10:34:37 +0530

    shell scripting

--- 13-functions.sh
+++ 13-functions.sh
@@ -23,27 +23,35 @@ fi
 dnf list installed git
 
 VALIDATE $? "Listing Git "
-# if [ $? -ne 0 ]
-# then
-#     echo " Git is not installed, going to install it.. "
-#     dnf install git -y
-#     else
-#     echo "Git is already installed nothing to do.. "
-# fi    
+if [ $? -ne 0 ]
+then
+    echo " Git is not installed, going to install it.. "
+    dnf install git -y
+    if [ $? -ne 0 ]
+    then
+        echo " Git isntallation is not sucess..check it"
+        exit 1
+    else
+        echo " Git installation is scuess"
+    fi
+else           
+        echo "Git is already installed nothing to do.. "
 
-# dnf list installed mysql
+fi    
 
-# if  [ $? -ne 0 ]
-# then
-#     echo " My sql is not installed. going to install it.."
-#     dnf install mysql -y
-#     if [ $? -ne 0 ]
-#     then    
-#         echo "MYSQL installation is failure..please check "
-#         exit 1
-#     else
-#         echo " MYSQL installation is sucess "    
-#     fi
-# else
-#     echo " MySQL is already installed..nothing to do.."    
-# fi        
+dnf list installed mysql
+
+if  [ $? -ne 0 ]
+then
+    echo " My sql is not installed. going to install it.."
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then    
+        echo "MYSQL installation is failure..please check "
+        exit 1
+    else
+        echo " MYSQL installation is sucess "    
+    fi
+else
+    echo " MySQL is already installed..nothing to do.."    
+fi        
