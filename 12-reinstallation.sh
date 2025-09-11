3cf7ef49205264173cbc8a6b09ab6e43a1e99ecb
Omi <Pampari.omprakash@gmail.com>
2025-09-11 07:18:06 +0530

    shell scripting

--- 12-reinstallation.sh
+++ 12-reinstallation.sh
@@ -18,3 +18,18 @@ then
     else
     echo "Git is already installed nothing to do.. "
 fi    
+
+dnf list installed mysql
+
+if  [ $? -ne 0 ]
+then
+    echo " My sql is not installed. going to install it.."
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then    
+        echo "MYSQL installation is failure..please check "
+        exit 1
+    else
+        echo " MYSQL installation is sucess "    
+    fi
+fi        
