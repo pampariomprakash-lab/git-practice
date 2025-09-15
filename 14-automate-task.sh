56a72d954df946e031027a222f5a2e3da27cc344
Omi <Pampari.omprakash@gmail.com>
2025-09-15 11:08:53 +0530

    shell scripting

--- 14-automate-task.sh
+++ 14-automate-task.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+#Sample Script: Automate a Daily Backup
+#wat to do
+#we need to create two varaiables to collect the log data and save it
+
+SOURCE_DIR="/home/om/porjects"
+BACKUP_DIR="/home/om/backups"
+DATE=$(date)
+echo " Starting backup..."
+tar -czf "$BACKUP_DIR/backup-$DATE. tar.gz" "$SOURCE_DIR"
+echo " Backup completed for $DATE"
